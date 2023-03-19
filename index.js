0d3101e062d6790f9c7f24231f2a019860a2deeb
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-03-19 13:03:06 -0300

    added middleware and router init

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-app.use(cors());
+//middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+//init
+app.use(express.json())
+app.use(cors())
+app.use('/public', express.static(`${process.cwd()}/public`))
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+//routers
+const urlShortenerRouter = require('./routes/url-shortener')
+
+//routes
+app.get('/', function (req, res) {
+	res.sendFile(process.cwd() + '/views/index.html')
+})
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/hello', function (req, res) {
+	res.json({ greeting: 'hello API' })
+})
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+app.use('/api/shorturl', urlShortenerRouter)
+
+//error handlers
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+app.listen(port, function () {
+	console.log(`Listening on port ${port}`)
+})
