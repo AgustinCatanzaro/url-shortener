56fc699b0210569ea57814ded16269fb031ab1d1
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-03-19 20:43:47 -0300

    added prettier format

--- index.js
+++ index.js
@@ -3,16 +3,27 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-// Basic Configuration
-const port = process.env.PORT || 3000
+// connectDB
+const connectDB = require('./db/connect')
 
 //middleware
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 //init
+
 app.use(express.json())
-app.use(cors())
+app.use(
+	cors({
+		origin: '*',
+		methods: 'POST, GET',
+		// credentials: true,
+		// allowedHeaders: 'Content-Type',
+		// preflightContinue: false,
+	})
+)
+// app.set('trust proxy', 1)
+
 app.use('/public', express.static(`${process.cwd()}/public`))
 
 //routers
@@ -34,6 +45,19 @@ app.use('/api/shorturl', urlShortenerRouter)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-app.listen(port, function () {
-	console.log(`Listening on port ${port}`)
-})
+// Basic Configuration
+const port = process.env.PORT || 3000
+
+//added db connection
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI)
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}...`)
+		)
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+start()
