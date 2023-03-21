4836dbdb8677c177423f30f224f3830f1091df3c
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-03-21 03:38:10 -0300

    recoded all logic to be inside the dns.lookup callback. added redirect to shorturl

--- controllers/url-shortener.js
+++ controllers/url-shortener.js
@@ -1,47 +1,70 @@
 const UrlDB = require('../models/urls')
+const dns = require('dns')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../errors')
 
-const urlShortener = async (req, res) => {
-	// agregar lo de hostname si la url tiene el formato correcto pero no existe.
-
+const urlShortener = (req, res) => {
 	const { url: requestedUrl } = req.body
-	console.log(requestedUrl)
 
-	//before accessing db need to check if the request url is a valid one.
-	const checkUrl = new URL(requestedUrl)
-	if (!checkUrl) {
-		throw new BadRequestError('invalid url')
-	}
+	const parsedUrl = new URL(requestedUrl)
 
-	//checking if the url already exists in the db.
-	let url = await UrlDB.findOne({
-		url: requestedUrl,
+	//before accessing db need to check if the request url is a valid one/exists.
+	dns.lookup(parsedUrl.hostname, async (err) => {
+		if (!err) {
+			//checking if the url exists in the db
+			const dbUrl = await UrlDB.findOne({
+				url: parsedUrl.origin,
+			})
+			//if the url already exists in the db, we send it back and its short url reference.
+			if (dbUrl) {
+				const { url: originalUrl, shortref: assignedRef } = dbUrl
+				res.status(StatusCodes.OK).json({
+					original_url: originalUrl,
+					short_url: assignedRef,
+				})
+			} else {
+				//if the url doesnt exists we search the highest shortReference
+				const dbMaxShortRef = await UrlDB.find()
+					.sort('-shortref')
+					.select('shortref')
+					.limit(1)
+				const greatestShortref = dbMaxShortRef[0].shortref + 1
+
+				//we create a new entry in the db with the received url and the  HighestReference in the db +1
+				await UrlDB.create({
+					url: parsedUrl.origin,
+					shortref: greatestShortref,
+				})
+				console.log(
+					`${parsedUrl.origin} added to the db with the short url: ${greatestShortref}`
+				)
+				//we send a response with the url and the reference saved in the db
+				res.status(StatusCodes.OK).json({
+					original_url: parsedUrl.origin,
+					short_url: greatestShortref,
+				})
+			}
+		} else {
+			//if the url is invalid/doesnt exists we send an error response.
+			res.status(StatusCodes.BAD_REQUEST).json({ error: 'Invalid Hostname' })
+		}
 	})
-	//if the url already exists in the db, we send it back and its shor url reference.
-	if (url) {
-		const { url: orignalUrl, shortref: assignedRef } = url
+}
+
+const getShortUrl = async (req, res) => {
+	const { shortref: receivedReference } = req.params
+	const dbRef = await UrlDB.findOne({
+		shortref: receivedReference,
+	})
+	//checking if the short url exists in the db and redirecting to it.
+	if (dbRef) {
+		const { url: urlRedirect } = dbRef
+		res.redirect(urlRedirect)
+	} else {
+		// if the short url doesnt exist sending response with the error.
 		res.status(StatusCodes.OK).json({
-			original_url: orignalUrl,
-			short_url: assignedRef,
-		})
-	} //if the url doesnt exist, we get the greater shortref created in the db and create a new element with the new url and the last shortref+1
-	else {
-		url = await UrlDB.find().sort('-shortref').select('shortref').limit(1)
-		const greatestShortref = url[0].shortref + 1
-		await UrlDB.create({ url: requestedUrl, shortref: greatestShortref })
-		console.log(
-			`${requestedUrl} added to the db with the short url: ${greatestShortref}`
-		)
-		res.status(StatusCodes.OK).json({
-			original_url: requestedUrl,
-			short_url: greatestShortref,
+			error: 'No short URL found for the given input',
 		})
 	}
-}
-//prob async
-const getShortUrl = (req, res) => {
-	res.send('getShortUrl controller')
 }
 
 module.exports = { urlShortener, getShortUrl }
